b613fba676d4956e30d631eb2c8db273be8eb5a4
AniDoesCoding <u24808840@tuks.co.za>
2025-09-02 21:36:19 +0200

    Part 1

--- backend/server.js
+++ backend/server.js
@@ -5,5 +5,5 @@ const app = express();
 app.use(express.static("public"));
 
 app.listen(1337, () => {
-console.log("Listening on localhost:1337");
+console.log("Listening on https://localhost:1337");
 });
