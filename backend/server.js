89ec568a125f5a939f881ce0c3ca150eea2b132e
AniDoesCoding <u24808840@tuks.co.za>
2025-09-03 20:56:09 +0200

    I don't know what I'm doing but things are vaguely working

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,18 @@
-import express from "express"
-
+const express = require('express');
+const path = require('path');
 const app = express();
+const port = 3000;
 
-app.use(express.static("public"));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, '../dist')));
 
-app.listen(1337, () => {
-console.log("Listening on https://localhost:1337");
+app.get('/api', (rep, res) => {
+    res.json({ message: "Hello World"})
+});
+
+app.use(express.static(path.join(__dirname, '../dist')));
+app.get('/{=any}', (req, res) => res.sendFile(path.join(__dirname, '../frontend/public/index.html')));
+
+app.listen(port, () => {
+console.log("Listening on http://localhost:3000");
 });
